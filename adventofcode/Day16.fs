67aad24e834249ab92651a35a283e674ed7e5ef9
dawe <dawedawe@gmx.de>
2018-01-13 12:57:27 +0100

    Day 16

---
+++ adventofcode/Day16.fs
@@ -0,0 +1,56 @@
+module Day16
+
+open System
+
+type Move =
+    | S of int
+    | X of (int * int)
+    | P of (char * char)
+
+let spin dancers n =
+    let startPos = Array.length dancers - n
+    let dancersToMove = dancers.[startPos ..]
+    Array.append dancersToMove dancers.[0 .. (startPos - 1)]
+
+let exchange (dancers : 'T array) p1 p2 =
+    let t = dancers.[p1]
+    dancers.[p1] <- dancers.[p2]
+    dancers.[p2] <- t
+    dancers
+
+let partner (dancers : char array) name1 name2 =
+    let name1Pos = Array.findIndex (fun x -> x = name1) dancers
+    let name2Pos = Array.findIndex (fun x -> x = name2) dancers
+    exchange dancers name1Pos name2Pos
+
+let makeMove (dancers : char array) move =
+    match move with
+    | S n -> spin dancers n
+    | X (pos1, pos2) -> exchange dancers pos1 pos2
+    | P (name1, name2) -> partner dancers name1 name2
+
+let dance moves =
+    let mutable dancers = [for x in [97 .. 112] do yield char(x)] |> List.toArray
+    for m in moves do
+        dancers <- makeMove dancers m
+    Array.fold (fun x y -> string(x) + string(y)) "" dancers
+
+let parseMove (moveString : string) =
+    let moveType = moveString.[0]
+    let args = moveString.[1..].Split [|'/'|]
+    match moveType with
+    | 's' -> S (int args.[0])
+    | 'p' -> P (args.[0].[0], args.[1].[0])
+    | 'x' -> X ((int args.[0]), (int args.[1]))
+    | _ -> raise(Exception("unknown move"))
+
+let parseMoves (input : string) =
+    let moveStrings = input.Split [|','|]
+    Array.map parseMove moveStrings
+
+let day16 () =
+    let input = System.IO.File.ReadAllText "Day16Input.txt"
+    let moves = parseMoves input
+    let dancers = dance moves
+    dancers
+    
