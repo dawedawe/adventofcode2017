53cdc1d26d91545b6925665e97a2178e4f79a722
dawe <dawedawe@gmx.de>
2018-03-11 00:35:30 +0100

    Day 4 Part 2

--- adventofcode/Day4.fs
+++ adventofcode/Day4.fs
@@ -6,12 +6,27 @@ module Day4
     let getLines path =
         System.IO.File.ReadAllLines path
 
-    let isValidPhrase (s : String) =
+    let isValidPhrase (s : string) =
         let words = s.Split ' '
         let wordsSet = new HashSet<string>(words)
         Array.length words = wordsSet.Count
 
+    let containsAnagram (s : string) =
+        let words = s.Split ' '
+        let wordArrays = Array.map (fun (w : string) -> Array.sort(w.ToCharArray())
+                                                        |> Array.fold (fun x y -> x + string(y)) "") words
+        let wordsSet = new HashSet<string>(wordArrays)
+        Array.length words <> wordsSet.Count
+
+    let isValidPhrasePart2 (s : string) =
+        isValidPhrase s && not (containsAnagram s)
+
     let countValidPassphrases () =
         let lines = getLines "Day4Input.txt"
         let validPhrases = Array.filter isValidPhrase lines
+        Array.length validPhrases
+
+    let countValidPassphrasesPart2 () =
+        let lines = getLines "Day4Input.txt"
+        let validPhrases = Array.filter isValidPhrasePart2 lines
         Array.length validPhrases
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -4,6 +4,6 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let r = Day3.day3Part2 ()
+    let r = Day4.countValidPassphrasesPart2 ()
     printfn "%A" r
     0 // return an integer exit code
