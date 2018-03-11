f3b9867f0175ccecea561b672cad32bea9349143
dawe <dawedawe@gmx.de>
2018-03-11 11:01:33 +0100

    Day 6 Part 2

--- adventofcode/Day6.fs
+++ adventofcode/Day6.fs
@@ -12,18 +12,22 @@ let distributeMemoryBlock (banks : int array) indexToDistribute =
     newConfig
 
 let rec distributeMemoryHelper seenConfigs currentConfig cycles =
+    let cycles' = cycles + 1
     let firstMax = Array.max currentConfig
     let firstMaxIndex = Array.findIndex (fun x -> x = firstMax) currentConfig
     let newConfig = distributeMemoryBlock currentConfig firstMaxIndex
-    if not (Array.contains newConfig seenConfigs)
+    if not (Array.contains newConfig (Array.map fst seenConfigs))
     then
-        let newSeenConfigs = Array.append seenConfigs [|newConfig|]
-        distributeMemoryHelper newSeenConfigs newConfig (cycles + 1)
-    else cycles + 1  
+        let newSeenConfigs = Array.append seenConfigs [|(newConfig, cycles')|]
+        distributeMemoryHelper newSeenConfigs newConfig (cycles')
+    else
+        let alreadySeen = Array.find (fun a -> fst a = newConfig) seenConfigs
+        let loopSize = cycles' - (snd alreadySeen)
+        cycles', loopSize
 
 let distributeMemory startingConfig =
     let current = Array.copy startingConfig
-    distributeMemoryHelper [|startingConfig|] current 0
+    distributeMemoryHelper [|(startingConfig, 0)|] current 0
 
 let day6 () =
     let startConfig = [|10; 3; 15; 10; 5; 15; 5; 15; 9; 2; 5; 8; 5; 2; 3; 6|]
