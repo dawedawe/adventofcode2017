e974d3b0d88bccc25d08c43db61cf4ae564a1819
dawe <dawedawe@gmx.de>
2018-03-18 15:17:45 +0100

    Day 10 Part 2

--- adventofcode/Day10.fs
+++ adventofcode/Day10.fs
@@ -47,3 +47,45 @@ let day10 () =
     let lengths = [| 31; 2; 85; 1; 80; 109; 35; 63; 98; 255; 0; 13; 105; 254; 128; 33 |]
     let r = day10helper numbers lengths
     r.[0] * r.[1]
+
+[<Literal>]
+let Input = "31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33"
+
+let stringToAscii (s : string) =
+    s.ToCharArray()
+    |> Array.map int
+
+
+let day10helperPart2 input lengths position skip =
+    let mutable nums = input
+    let mutable pos = position
+    let mutable skipSize = skip
+    for l in lengths do
+        nums <- reverse nums l pos
+        pos <- (pos + l + skipSize) % input.Length
+        skipSize <- skipSize + 1
+    nums, pos, skipSize
+
+let xorParts (hash : int array) =
+    let mutable values = Array.empty
+    for i in [0 .. 15] do
+        let part = Array.skip (i * 16) hash |> Array.take 16
+        let v = Array.fold (^^^) 0 part
+        values <- Array.append values (Array.singleton v)
+    values
+
+let day10Part2 () =
+    let numbers = Array.init 256 id
+    let asciiCodes = stringToAscii Input
+    let lengths = Array.append asciiCodes [|17; 31; 73; 47; 23|]
+    let mutable hash = numbers
+    let mutable pos = 0
+    let mutable skip = 0
+    for _ in [1 .. 64] do
+        let h, p, s = day10helperPart2 numbers lengths pos skip
+        hash <- h
+        pos <- p
+        skip <- s
+    let denseHash = xorParts hash
+    let denseHashHexValues = Array.map (sprintf "%02x") denseHash
+    Array.fold (+) "" denseHashHexValues
