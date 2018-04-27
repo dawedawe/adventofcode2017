149b2602088782397e6bab84eba8bd0fac129429
dawe <dawedawe@gmx.de>
2018-04-27 22:24:24 +0200

    Day 14 Part 2

--- adventofcode.test/Day14Tests.fs
+++ adventofcode.test/Day14Tests.fs
@@ -1,3 +1,21 @@
 module Day14Tests
 
 open Xunit
+open Day14
+
+[<Fact>]
+let findStartingPosTest1 () =
+    let matrix = Array2D.create 4 4 "."
+    let startingPos = findStartPos matrix
+    Assert.True (Option.isNone startingPos)
+    matrix.[3,3] <- "#"
+    let startingPos2 = findStartPos matrix
+    Assert.True (Option.isSome startingPos2)
+    Assert.True (startingPos2.Value = (3,3))
+
+[<Fact>]
+let findStartingPosTest2 () =
+    let matrix = Array2D.create 4 4 "#"
+    let startingPos = findStartPos matrix
+    Assert.True (Option.isSome startingPos)
+    Assert.True (startingPos.Value = (0, 0))
--- adventofcode/Day14.fs
+++ adventofcode/Day14.fs
@@ -75,15 +75,68 @@ let bitPattern n =
         bits <- Array.append [|bitSet|] bits
     bits
 
+[<Literal>]
+let Input = "jxqlasbh"
+
 let day14 () =
-    let input = "jxqlasbh"
     let mutable sum = 0
     for r in [0 .. 127] do
-        let rowInput = input + "-" + string(r)
+        let rowInput = Input + "-" + string(r)
         let h = hash rowInput
         let bits = Array.map bitPattern h |> Array.collect id
         let bitCount = Array.filter (fun b -> b = 1) bits
         sum <- sum + bitCount.Length
     sum
-        
-        
+
+let findStartPos matrix =
+    let mutable startPos = Option.None
+    let mutable r = 0
+    let mutable c = 0
+    while r < Array2D.length1 matrix && Option.isNone startPos do
+        while c < Array2D.length2 matrix && Option.isNone startPos do
+            if matrix.[r, c] = "#"
+            then startPos <- Option.Some (r, c)
+            c <- c + 1
+        c <- 0
+        r <- r + 1
+    startPos
+
+let adjacentRegionPositions (matrix : string [,]) startPos =
+    let legalPos (x, y) =
+        x >= 0 && x < Array2D.length1 matrix
+            && y >= 0 && y < Array2D.length2 matrix
+             && matrix.[x, y] = "#"
+    let r, c = startPos
+    let upper = r - 1, c
+    let lower = r + 1, c
+    let left = r, c - 1
+    let right = r, c + 1
+    Array.filter legalPos [|upper; lower; left; right|]
+
+let rec setRegion (matrix : string [,]) regionNumber startPos =
+    matrix.[fst startPos, snd startPos] <- string(regionNumber)
+    let adjacentRegPositions = adjacentRegionPositions matrix startPos
+    for p in adjacentRegPositions do
+        setRegion matrix regionNumber p
+
+let setRegions (matrix : string [,]) =
+    let mutable startPos = findStartPos matrix
+    let mutable regionNumber = 0
+    while Option.isSome startPos do
+        regionNumber <- regionNumber + 1
+        setRegion matrix regionNumber startPos.Value
+        startPos <- findStartPos matrix
+    regionNumber
+
+let day14Part2 () =
+    let matrix = Array2D.create 128 128 "."
+    for r in [0 .. 127] do
+        let rowInput = Input + "-" + string(r)
+        let h = hash rowInput
+        let bits = Array.map bitPattern h |> Array.collect id
+        for c in [0 .. 127] do
+            if bits.[c] = 1
+            then matrix.[r, c] <- "#"
+    let lastRegionNumber = setRegions matrix
+    lastRegionNumber
+    
